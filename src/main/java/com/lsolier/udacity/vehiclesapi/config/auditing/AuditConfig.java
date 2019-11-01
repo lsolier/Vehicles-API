e370aa7d310c41795f04f5ea5991f763ef6283fd
Luis Solier Sajami <luis.sajami@gmail.com>
2019-11-01 18:08:49 -0500

    fix unit test and audit config

--- src/main/java/com/lsolier/udacity/vehiclesapi/config/auditing/AuditConfig.java
+++ src/main/java/com/lsolier/udacity/vehiclesapi/config/auditing/AuditConfig.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import java.util.UUID;
 
 @Configuration
-@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 public class AuditConfig {
 
   @Bean
--- src/test/java/com/lsolier/udacity/vehiclesapi/VehiclesApiApplicationTests.java
+++ src/test/java/com/lsolier/udacity/vehiclesapi/VehiclesApiApplicationTests.java
@@ -1,13 +1,8 @@
 package com.lsolier.udacity.vehiclesapi;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class VehiclesApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
