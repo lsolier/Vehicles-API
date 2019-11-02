e1d44cf9b9a83100f51b70cc22ae27f2adca663a
Luis Solier Sajami <luis.sajami@gmail.com>
2019-11-01 22:14:04 -0500

    adding controller and handle exception for controller

---
+++ src/main/java/com/lsolier/udacity/vehiclesapi/api/ApiError.java
@@ -0,0 +1,21 @@
+package com.lsolier.udacity.vehiclesapi.api;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiError {
+
+  private String message;
+  private List<String> errors;
+
+}
