d2fc0a0075b2409615ca19cd24e9558c6e4c0347
Luis Solier Sajami <luis.sajami@gmail.com>
2019-11-02 22:42:12 -0500

    fix unit test

--- src/test/java/com/lsolier/udacity/vehiclesapi/VehiclesApiApplicationTests.java
+++ src/test/java/com/lsolier/udacity/vehiclesapi/VehiclesApiApplicationTests.java
@@ -4,9 +4,9 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class VehiclesApiApplicationTests {
+public class VehiclesApiApplicationTests {
 
   @Test
-  void contextLoads() {
+  public void contextLoads() {
   }
 }
